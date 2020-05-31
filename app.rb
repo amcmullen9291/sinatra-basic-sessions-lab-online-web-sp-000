70517981b6f3fc8f2c4ca365795cf9b94813ed31
anthony mcmullen <jondouglasheit@gmail.com>
2020-05-31 19:51:39 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,8 +2,16 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   
+  configure do
+    enable :sessions
+    session_secret
+  end 
+  
   get '/' do 
     erb :index
   end 
 
+  post '/checkout' do 
+    @item = params[:item]
+    
 end
